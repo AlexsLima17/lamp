e9e22c6e321ffa82722ac07ec5a149224828c6ac
AlexsLima17 <alexsantoslima007@gmail.com>
2024-11-07 20:44:22 -0300

    lampada

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
  */
 
 function quebrar() {
-    document.getElementById('lamp').scr="img/broken.jpg"
     //reproduzindo um arquivo de audio de JS
     //Passo 1: copiar o arquivo de áudio para o projeto 
     //Passo 2: Usar a classe Audio (biblioteca interna do JS)
-    
+
     let som = new Audio()
     som.src = "sound/glassbreaking.wav"
     som.play()
+    document.getElementById('lamp').scr="img/broken.jpg"
 }
