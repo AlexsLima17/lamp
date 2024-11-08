382ff2fc39fa59f3612e6d1f904b627abe77ace0
AlexsLima17 <alexsantoslima007@gmail.com>
2024-11-07 21:57:59 -0300

    lampada

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
  * @author Alex Lima
  */
 
+//Variáveis de apoio lógica
+let chave = false
+
 function quebrar() {
     //reproduzindo um arquivo de audio de JS
     //Passo 1: copiar o arquivo de áudio para o projeto 
@@ -11,5 +14,19 @@ function quebrar() {
     let som = new Audio()
     som.src = "sound/glassbreaking.wav"
     som.play()
-    document.getElementById('lamp').scr="img/broken.jpg"
+    document.getElementById('lamp').src="img/broken.jpg"
+}
+
+function onoff(){
+    if (chave === false){
+        // ligar a chave
+        document.getElementById('interruptor').src="img/swon.png" 
+        chave = true //O JS agorasabe que a chave está ligada
+        //acender a lampada
+        document.getElementById('lamp').src="img/on.jpg"
+    }else {
+        document.getElementById('interruptor').src="img/swoff.png"
+        chave = false
+        document.getElementById('lamp').src="img/off.jpg"
+    }
 }
