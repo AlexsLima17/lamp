dc5d8d881208d131d6d0aeb54bd3dd30afc1a083
AlexsLima17 <alexsantoslima007@gmail.com>
2024-11-07 22:17:29 -0300

    lampada

--- app.js
+++ app.js
@@ -3,30 +3,32 @@
  * @author Alex Lima
  */
 
-//Variáveis de apoio lógica
 let chave = false
+let lampada = true
+
 
 function quebrar() {
-    //reproduzindo um arquivo de audio de JS
-    //Passo 1: copiar o arquivo de áudio para o projeto 
-    //Passo 2: Usar a classe Audio (biblioteca interna do JS)
-
-    let som = new Audio()
-    som.src = "sound/glassbreaking.wav"
-    som.play()
-    document.getElementById('lamp').src="img/broken.jpg"
+    if (lampada === true) {
+        //reproduzindo um arquivo de audio do JS
+        // passo 1: copiar o arquivo de audio para o projeto
+        // passo 2: usae a classe Audio(biblioteca interna do JS)
+        let som = new Audio()
+        som.src = "sound/glassbreaking.wav"
+        som.play()
+        //apoio a logica para o JS identificar a lampada
+        document.getElementById('lamp').src = "img/broken.jpg"
+        lampada = false
+    }
 }
 
-function onoff(){
-    if (chave === false){
-        // ligar a chave
-        document.getElementById('interruptor').src="img/swon.png" 
-        chave = true //O JS agorasabe que a chave está ligada
-        //acender a lampada
-        document.getElementById('lamp').src="img/on.jpg"
-    }else {
-        document.getElementById('interruptor').src="img/swoff.png"
+function onoff() {
+    if (chave === false && lampada === true) {
+        document.getElementById('interruptor').src = "img/swon.png"
+        document.getElementById('lamp').src = "img/on.jpg"
+        chave = true //chave ligada
+    } else if (lampada === true) {
+        document.getElementById('interruptor').src = "img/swoff.png"
+        document.getElementById('lamp').src = "img/off.jpg"
         chave = false
-        document.getElementById('lamp').src="img/off.jpg"
     }
 }
